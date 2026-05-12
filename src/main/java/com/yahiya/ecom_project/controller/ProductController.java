6ceafa943af84bab4d0708b4eeea51a378bde1ba
yahiya <yahiyakhan85013@gmail.com>
2026-05-12 15:11:50 +0530

    Initial commit

---
+++ src/main/java/com/yahiya/ecom_project/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.yahiya.ecom_project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "hellow";
+    }
+}
